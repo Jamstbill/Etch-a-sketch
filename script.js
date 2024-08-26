5f74b12ec5e89ce940144f2ff88d7c898e1798b8
Jamie <jamiehanson2012@gmail.com>
2024-08-26 15:32:23 +0100

    Added dynamic grid creation and colour picker

--- script.js
+++ script.js
@@ -1,17 +1,64 @@
 const container = document.querySelector('.container');
-const gridSize = 50;
-const squareSize =  container.clientWidth / gridSize;
+let gridSize;
+let selectedColour = 'white';
 
-for (i = 0; i < gridSize * gridSize; i++){
-    const square = document.createElement('div');
-    square.className = 'square';
-    square.style.width = `${squareSize}px`
-    square.style.height = `${squareSize}px`
-    container.appendChild(square);
+const red = document.querySelector('.red');
+red.addEventListener('click', () => {
+    selectedColour = 'red';
+})
+const blue = document.querySelector('.blue');
+blue.addEventListener('click', () => {
+    selectedColour = 'blue';
+})
+const yellow = document.querySelector('.yellow');
+yellow.addEventListener('click', () => {
+    selectedColour = 'yellow';
+})
+const green = document.querySelector('.green');
+green.addEventListener('click', () => {
+    selectedColour = 'green';
+})
+const pink = document.querySelector('.pink');
+pink.addEventListener('click', () => {
+    selectedColour = 'pink';
+})
+const eraser = document.querySelector('.eraser');
+eraser.addEventListener('click', () => {
+    selectedColour = 'white';
+})
+const purple = document.querySelector('.purple');
+purple.addEventListener('click', () => {
+    selectedColour = 'purple';
+})
 
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'red';
-    });
+const button = document.querySelector('.btn');
+button.addEventListener('click', () => {
+    gridSize = document.querySelector('.input').value;
+    if(gridSize <= 100){
+        drawGrid(gridSize);
+    } else
+    alert('Grid can only be 100 * 100');
+})
+
+function drawGrid(gridSize = 16){
+    container.innerHTML = '';
+    const squareSize =  container.clientWidth / gridSize;
+
+    for (i = 0; i < gridSize * gridSize; i++){
+        let chosenColour;
+        const square = document.createElement('div');
+
+        square.className = 'square';
+        square.style.width = `${squareSize}px`
+        square.style.height = `${squareSize}px`
+        container.appendChild(square);
+
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = selectedColour;
+        });
+    }
 }
+
+drawGrid(gridSize);
 
 
